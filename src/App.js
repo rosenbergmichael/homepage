0c6cf5c48c6c358d64b98ab83105c11983e52d33
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-01-25 15:00:53 -0500

    imported browser router, route, switch from react-router-dom and set up routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-
-      
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
       </React.Fragment>
       );
   }
