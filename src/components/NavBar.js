993ffc0c710fb01c17f119d0bca1bbf1001e15ce
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-01-27 16:32:12 -0500

    added styling for NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,24 @@
 import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 import styled from 'styled-components';
+
+
+const Styles = styled.div`
+  .navbar {
+    background-color: #222;
+  }
+
+  .navbar-brand, .navbar-nav .nav-link {
+    color: #bbb;
+
+    &:hover {
+      color: white; 
+    }
+  }
+`;
+
+export const NavBar = () => (
+   <Styles>
+     
+   </Styles>
+)
