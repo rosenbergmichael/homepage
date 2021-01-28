cb1a9f72a3ea8abf5b53c3cc68dce2590679b05b
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-01-28 10:10:50 -0500

    imported NavBar in App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { About } from './About';
 import { Contact } from './Contact';
 import { NoMatch } from './NoMatch';
 import { Layout } from './components/Layout';
+import { NavBar } from './components/NavBar';
 
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <NavBar />
         <Layout>
           <Router>
             <Switch>
