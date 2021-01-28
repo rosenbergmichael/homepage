cdb90d8f7d53517413e5a81d8a99ba40ab9c1140
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-01-28 10:07:46 -0500

    built out NavBar component with styling and links to different pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -19,6 +19,16 @@ const Styles = styled.div`
 
 export const NavBar = () => (
    <Styles>
-     
+    <Navbar expand="lg">
+      <Navbar.Brand href="/">Code Life</Navbar.Brand>  
+      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
+          <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
+          <Nav.Item><Nav.Link href="/contact">Contact</Nav.Link></Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar> 
    </Styles>
 )
