cc4825d7a2143895163204d5b87634d55a68be9b
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-01-29 13:53:59 -0500

    added info on About Me page

--- src/Contact.js
+++ src/Contact.js
@@ -4,6 +4,7 @@ export const Contact = () => (
     <div>
       <br></br>
       <h2>Contact Info- Feel Free to Reach Out!</h2>
+      <br></br>
       <p>E: rosenbergmj1@gmail.com</p>
       <p>C: 862-219-0320</p>
     </div>
