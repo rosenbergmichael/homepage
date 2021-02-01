bf470c06883231afbd0b6ea9a705ce88b3c19ddf
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-02-01 14:07:51 -0500

    added overlay to jumbotron image

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -13,6 +13,18 @@ const Styles = styled.div `
     position: relative;
     z-index: -2;
   }
+
+  .overlay {
+    background-color: #000;
+    opacity: 0.6;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+  }
+
 `;
 
 export const Jumbotron = () => (
